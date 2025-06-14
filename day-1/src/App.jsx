ade26d643e868038d10bc0522029f8c45e5fd282
Imran Ahmed <imran.techvoyager@gmail.com>
2025-06-14 23:30:29 +0530

    feat(Ui): Add components -- Header, Main and Footer

--- day-1/src/App.jsx
+++ day-1/src/App.jsx
@@ -1,10 +1,16 @@
+import Footer from "./components/Footer"
 import Greet from "./components/Greet"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
 function App() {
 
   return (
     <section>
+      <Header />
       Hello world!!
       <Greet />
+      <MainContent />
+      <Footer />
     </section>
   )
 }
---
+++ day-1/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h1>Main content</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse laudantium eos nesciunt iste corrupti excepturi saepe. Ut, illo? Numquam placeat commodi distinctio ducimus provident. Qui sunt mollitia vel dolores saepe recusandae quisquam expedita repudiandae? Dolorum nam rerum minima, repudiandae deleniti aliquid similique placeat pariatur! Consequuntur in, dolore id repellat earum quisquam adipisci, ut est fugit vel harum dolorem rem, dicta veniam odio ducimus error architecto aut quas itaque eligendi tempora. Harum fugiat, sequi soluta libero non similique reprehenderit perferendis, cum nisi, molestiae quae cupiditate deleniti possimus. Eveniet, provident? Animi, autem necessitatibus. Reiciendis assumenda asperiores iste laudantium eveniet eaque, non ex necessitatibus quae neque magnam aperiam dolorem? Dolorem et a quam consequuntur eius minus quo, dolor nisi cumque. Ipsum, ratione, atque obcaecati cum veniam optio rem quas minus sit sapiente recusandae ex repudiandae perspiciatis esse vel, ab provident error vero incidunt quisquam iste corporis laborum culpa? Ratione cupiditate minima vero aliquam placeat. Distinctio impedit accusantium delectus vitae aperiam laudantium aliquid repellat molestiae blanditiis pariatur. Recusandae, nulla eius tempora magnam, perferendis doloribus quam quidem, a cupiditate quasi et nostrum magni itaque totam minus quaerat voluptas odit! Neque aut incidunt corporis repudiandae sunt, a possimus odio commodi, doloribus, dolor reiciendis nostrum provident voluptates?</p>
+    </main>
+  )
+}
+
+export default MainContent
