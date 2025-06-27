062b689a55957fc1dad4c295ca5fac272efcfdef
Imran Ahmed <imran.techvoyager@gmail.com>
2025-06-28 01:39:59 +0530

    feat(contextApi): implement context providers for name and age in App component

--- day-2/src/App.jsx
+++ day-2/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ComponentA from './propdrill/ComponentA'
+import { createContext } from 'react';
+
+export const context = createContext();
+export const context1 = createContext();
 
 const App = () => {
   const name = "imran";
-  
+  const age = 23;
+
   return (
-    <div>
-      <ComponentA name={name}/>
-    </div>
+    <context.Provider value={name}>
+      <context1.Provider value={age}>
+        <ComponentA />
+      </context1.Provider>
+    </context.Provider>
   )
 }
 
--- day-2/src/propdrill/ComponentC.jsx
+++ day-2/src/propdrill/ComponentC.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { context } from '../App'
+import { context1 } from '../App'
 
-const ComponentC = ({name}) => {
+const ComponentC = () => {
   return (
-    <div>
-        <h1>my name is {name}</h1>
-    </div>
+    <context.Consumer>
+        {(name) => {
+            return <context1.Consumer>
+                {(age) => (
+                    <h1>my name is {name} and im {age} years old</h1>
+                )}
+            </context1.Consumer>
+        }}
+    </context.Consumer>
   )
 }
 
