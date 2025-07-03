c900bbce38d4c0511e5c1fd375b7e72eae2d746e
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-04 00:30:26 +0530

    feat(ui): add product prototyping

--- advance-filtering/src/Product/Product.jsx
+++ advance-filtering/src/Product/Product.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./Product.css";
+import { AiFillStar } from 'react-icons/ai';
+import { BsFillBagHeartFill } from "react-icons/bs";
+
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <section className='card-container'>
+      <section className='card'>
+        <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="shoe" />
+        <div className="card-details">
+          <h3 className='card-title'>Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del> 200
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill />
+            </div>
+          </section>
+        </div>
+      </section>
+    </section>
   )
 }
 
