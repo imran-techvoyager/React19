49f36f6cb9d620c799397b4ba11b75e4f7bd08fa
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-05 03:52:41 +0530

    feat(tuples): add tuple example with string, number, and boolean types

---
+++ ts-plaground/src/tuples.ts
@@ -0,0 +1,4 @@
+let myArr: [string, number, boolean];
+myArr = ["imran", 786, true];
+
+console.log(myArr);
--- ts-plaground/src/union-type.ts
+++ ts-plaground/src/union-type.ts
@@ -16,3 +16,12 @@ const user1: UserInfo | AccountDetails = {
 };
 
 console.log(`name: ${user1.name} and age: ${user1.age}`);
+
+//Literal types
+
+let color: "red" | "blue" | "green";
+let isTrue: true | false;
+let numbers: 1 | 2 | 3;
+
+color = "red";
+console.log(color);
