393f4e57ee6105e19fec30a940bc04844a6867d4
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-06 02:41:21 +0530

    feat(fetch): implement Fetch component to retrieve and display user data

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-
-type User = {
-  name: string;
-  username: string;
-}
 
 function App() {
-  const [data, setData] = useState<User[]>([])
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-       setTimeout(() => {   
-         axios.get("https://jsonplaceholder.typicode.com/users")
-         .then(res => {
-          setData(res.data)
-          setLoading(false)
-         })
-       }, 5000);
-  }, []);
-
-  if(loading){
-    return "Loading..."
-  }
 
   return (
-    <>
-      {data.map((user) => (
-        <div>         
-          <h1>{user.name}</h1>
-          <p>{user.username}</p>
-        </div>
-      ))}
-    </>
+    <div>
+      App
+    </div>
   )
 }
 
