550887c91b0f496fb6bd4855bc5c840cfd0311ff
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-05 16:46:04 +0530

    chore(data-fetch): data feting in reat

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+type User = {
+  name: string;
+  username: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<User[]>([])
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+       setTimeout(() => {   
+         axios.get("https://jsonplaceholder.typicode.com/users")
+         .then(res => {
+          setData(res.data)
+          setLoading(false)
+         })
+       }, 5000);
+  }, []);
+
+  if(loading){
+    return "Loading..."
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((user) => (
+        <div>         
+          <h1>{user.name}</h1>
+          <p>{user.username}</p>
+        </div>
+      ))}
     </>
   )
 }
